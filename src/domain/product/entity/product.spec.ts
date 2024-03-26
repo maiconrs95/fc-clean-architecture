92d3e4559cb77863660bd6e30991c0e3b3403496
Maicon Silva <maiconrs95@gmail.com>
2024-03-25 21:41:51 -0300

    feat: added product validator errors

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,23 +1,40 @@
 import Product from "./product";
+import ProductB from "./product-b";
 
 describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
-      const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
+      new Product("", "Product a", 100);
+    }).toThrowError("product: Id is required");
+    expect(() => {
+      new ProductB("", "Product b", 100);
+    }).toThrowError("product: Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
-      const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+      new Product("123", "", 100);
+    }).toThrowError("product: Name is required");
+    expect(() => {
+      new ProductB("456", "", 100);
+    }).toThrowError("product: Name is required");
   });
 
   it("should throw error when price is less than zero", () => {
     expect(() => {
-      const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+      new Product("123", "Product a", -1);
+    }).toThrowError("product: Price must be greater than zero");
+    expect(() => {
+      new ProductB("456", "Product b", -1);
+    }).toThrowError("product: Price must be greater than zero");
   });
+
+  it("should throw error when name, id and price are invalid", () => {
+    expect(() => {
+      new Product("", "", -1);
+    }).toThrowError("product: Id is required,product: Name is required,product: Price must be greater than zero");
+  });
+
 
   it("should change name", () => {
     const product = new Product("123", "Product 1", 100);
